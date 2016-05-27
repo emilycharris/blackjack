444aa6ca647efc2d5a0a17dfcc3b686a8390ca1a
emilycharris <emily.harris.mail@gmail.com>
2016-05-27 18:08:51 -0400

    revalue aces working

--- blackjack_hand.py
+++ blackjack_hand.py
@@ -2,10 +2,10 @@ from blackjack_deck import game_deck
 
 class Hand:
     def __init__(self):
-        self.value = 0
+        self.total_value = 0
         self.card_list = []
         self.card_list_values = []
-        self.total_value_non_aces = 0
+        self.card_list_values_no_aces = []
 
     def new_hand(self):
         while len(self.card_list) < 2:
@@ -21,41 +21,41 @@ class Hand:
         self.card_list.append(self.card)
         self.card_list_values.append(self.card_value)
         self.total_value = sum(self.card_list_values)
+        self.revalue_aces()
         print("You were dealt the following: ", self.card_list)
         print(self.card_list_values)
         print("The value of your hand is now ", self.total_value)
-        self.value += self.total_value
-        return self.total_value
+        return self.value
 
-    def non_aces(self):
-        for self.item in self.card_list_values:
-            if self.item != 1 and self.item != 11:
-                self.total_value_non_aces += self.item
-            else:
-                self.total_value_non_aces += 0
-        return self.total_value_non_aces
-
-    def ace_values(self):
-        self.non_aces()
-        if self.face[0] == 'A':
-            if self.total_value_non_aces <= 10:
+    def initial_ace_values(self):
+            if sum(self.card_list_values_no_aces) <= 10:
                 self.value = 11
             else:
                 self.value = 1
-        return self.value
+            return self.value
 
     def card_values(self):
         if self.face[0] in ['1', 'J', 'Q', 'K']:
             self.value = 10
+            self.card_list_values_no_aces.append(self.value)
         elif self.face[0] in ['2', '3', '4', '5', '6', '7', '8', '9']:
             self.value = int(self.face[0])
+            self.card_list_values_no_aces.append(self.value)
         else:
-            self.ace_values()
+            self.initial_ace_values()
         return self.value
+
+    def revalue_aces(self):
+        for self.i, self.item in enumerate(self.card_list_values):
+            if self.item == 11 or self.item == 1:
+                if sum(self.card_list_values_no_aces) <= 10:
+                    self.card_list_values[self.i] = 11
+                else:
+                    self.card_list_values[self.i] = 1
+
 
 
 hand = Hand()
 hand.new_hand()
 while hand.total_value < 21:
     hand.hit()
-
